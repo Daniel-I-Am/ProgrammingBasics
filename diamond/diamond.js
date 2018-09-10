f2e464dbb1a032761f13c2031b1c0d20ed404ecc
Daniel de Cloet <djdecloet@gmail.com>
2018-09-10 14:55:04 +0200

    draw added, shape wrong

--- diamond/diamond.js
+++ diamond/diamond.js
@@ -1,29 +1,43 @@
+shapeColor = '#0000ff'
+
 function drawADiamond() {
-    drawAShape("diamond");
+    initDraw("diamond");
 }
 
-function drawAShape(shapeName) {
+function initDraw(shapeName) {
+    //get canvas object
     canvas = document.getElementById("diamondCanvas");
-    console.log(canvas)
+    //fetch and draw
+    fetchShape(shapeName, function (shape) {draw(canvas, shape)});
 }
 
-function fetchShape(shapeName) {
-    fetch('./assets/' + shapeName + ".svg")
-        .then(
-            function(response) {
-                if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
-                    return;
-                }
+function fetchShape(shapeName, _callback) {
+    fetch("./svgs/" + shapeName + ".svg")
+            .then(response => response.text())
+            .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+            .then(data => _callback(data))
+}
 
-                // Examine the text in the response
-                response.json().then(function(data) {
-                    return data;
-                });
+function draw(canvas, shape) {
+    console.log("I need to work with this (", shape, ") on this (", canvas, ")")
+    svg = shape.children[0];
+    grid = svg.children[0];
+    scale = Math.min(canvas.width / Number(svg.attributes.width.value.slice(0, -2)), canvas.height / Number(svg.attributes.height.value.slice(0, -2)))
+    polygons = grid.children[0].children;
+    for (n in polygons) {
+        points = polygons[n].attributes.points.value.split(" ");
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = shapeColor;
+        ctx.beginPath();
+        for (pt in points) {
+            coords = points[pt].split(",");
+            if (pt == 0) {
+                ctx.moveTo(coords[0]*scale, coords[1]*scale);
+            } else {
+                ctx.lineTo(coords[0]*scale, coords[1]*scale);
             }
-        )
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err);
-        });
+        }
+        ctx.closePath();
+        ctx.fill();
+    }
 }
