007e2e3c49f48df3057558c02f351f3eda4e418c
Daniel de Cloet <djdecloet@gmail.com>
2018-09-10 14:09:16 +0200

    add fetch

--- diamond/diamond.js
+++ diamond/diamond.js
@@ -6,3 +6,24 @@ function drawAShape(shapeName) {
     canvas = document.getElementById("diamondCanvas");
     console.log(canvas)
 }
+
+function fetchShape(shapeName) {
+    fetch('./assets/' + shapeName + ".svg")
+        .then(
+            function(response) {
+                if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function(data) {
+                    return data;
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
