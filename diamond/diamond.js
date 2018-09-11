62d07eb3db7ff6000e7448bc79e126dbf44f860b
Daniel de Cloet <djdecloet@gmail.com>
2018-09-11 11:01:14 +0200

    var added

--- diamond/diamond.js
+++ diamond/diamond.js
@@ -1,5 +1,5 @@
-defaultCol = '#ffffff';
-selectedShape = "diamond";
+var defaultCol = '#ffffff';
+var selectedShape = "diamond";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -7,7 +7,7 @@ function sleep(ms) {
 
 function initDraw(shapeName) {    
     // get canvas object
-    canvas = document.getElementById("diamondCanvas");
+    var canvas = document.getElementById("diamondCanvas");
 
     // fetch and draw
     fetchShape(shapeName, function (shape) {draw(canvas, shape)});
@@ -22,26 +22,26 @@ async function fetchShape(shapeName, _callback) {
 }
 
 async function draw(canvas, shape) {
-    context = canvas.getContext('2d');
+    var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
     context = null;
     // draw according to params
     // get some clearer var names
-    svg = shape.children[0];
+    var svg = shape.children[0];
     // get scale to draw at (based on canvas size and svg size)
-    scale = Math.min(canvas.width / Number(svg.attributes.width.value.slice(0, -2)), canvas.height / Number(svg.attributes.height.value.slice(0, -2)))
+    var scale = Math.min(canvas.width / Number(svg.attributes.width.value.slice(0, -2)), canvas.height / Number(svg.attributes.height.value.slice(0, -2)))
     // get the polygons inside the svg
-    polygons = svg.children;
+    var polygons = svg.children;
     for (n in polygons) {
         // split the coords of the polygon
-        points = polygons[n].attributes.points.value.split(" ");
+        var points = polygons[n].attributes.points.value.split(" ");
         // get the canvas' context
         var ctx = canvas.getContext('2d');
         // set fill color
-        style = {}
-        polyStyle = polygons[n].attributes.style.value.split(";");
+        var style = {}
+        var polyStyle = polygons[n].attributes.style.value.split(";");
         for (i in polyStyle) {
-            styleAttr = polyStyle[i].split(":")
+            var styleAttr = polyStyle[i].split(":")
             if (styleAttr[1] == "none") {styleAttr[1] = defaultCol}
             style[styleAttr[0]] = styleAttr[1]
         }
@@ -53,7 +53,7 @@ async function draw(canvas, shape) {
         // go through all points
         for (pt in points) {
             // split up coords into x and y
-            coords = points[pt].split(",");
+            var coords = points[pt].split(",");
             if (pt == 0) {
                 // if it's the first point, move to without drawing line
                 ctx.moveTo(coords[0]*scale, coords[1]*scale);
