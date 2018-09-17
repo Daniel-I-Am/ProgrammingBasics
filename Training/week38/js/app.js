059f007e6eef341336de5e4ba55a7e2664f5d754
Daniel de Cloet <djdecloet@gmail.com>
2018-09-17 10:52:17 +0200

    adding more crap

--- Training/week38/js/app.js
+++ Training/week38/js/app.js
@@ -5,4 +5,21 @@ console.log(`Hello, I'm ${firstName} ${lastName} and I'm ${age} y.o.`);
 
 const priceOfApple = '0.59',
     numberOfApples = 200;
-console.log(`Price to pay is \$${Number(priceOfApple) * numberOfApples}`)
+console.log(`Price to pay is \$${Number(priceOfApple) * numberOfApples}`);
+
+let tempToday = 20,
+    tempYesterday = 25;
+if (tempToday > tempYesterday) {
+    console.log("Today was hotter than yesterday.");
+} else if (tempYesterday > tempToday) { 
+    console.log("Yesterday was hotter than today.");
+} else {
+    console.log("Today and yesterday were the same temperature");
+}
+
+let date = new Date();
+let message = "Today is " + date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+console.log(message);
+
+let college = "Please visit the HZ University of Applied Sciences";
+console.log(college.replace("Applied", "Mad"));
