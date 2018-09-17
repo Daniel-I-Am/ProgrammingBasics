faa655493da6c645c890d65cf306c9323f09abb9
Daniel de Cloet <djdecloet@gmail.com>
2018-09-17 10:21:53 +0200

    change proper input

--- Training/week38/js/app.js
+++ Training/week38/js/app.js
@@ -3,6 +3,6 @@ const firstName = "Daniel",
     age = 17;
 console.log(`Hello, I'm ${firstName} ${lastName} and I'm ${age} y.o.`);
 
-const priceOfApple = 0.59,
+const priceOfApple = '0.59',
     numberOfApples = 200;
-console.log(`Price to pay is \$${priceOfApple * numberOfApples}`)
+console.log(`Price to pay is \$${Number(priceOfApple) * numberOfApples}`)
