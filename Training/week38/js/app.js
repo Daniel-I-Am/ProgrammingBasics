c0896119c57e424dd8c3ea0596f5d6d73dcf96e6
Daniel de Cloet <djdecloet@gmail.com>
2018-09-17 10:10:15 +0200

    name and apples...

---
+++ Training/week38/js/app.js
@@ -0,0 +1,8 @@
+const firstName = "Daniel",
+    lastName = "de Cloet",
+    age = 17;
+console.log(`Hello, I'm ${firstName} ${lastName} and I'm ${age} y.o.`);
+
+const priceOfApple = 0.59,
+    numberOfApples = 200;
+console.log(`Price to pay is \$${priceOfApple * numberOfApples}`)
