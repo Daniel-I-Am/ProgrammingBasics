89a3b57a72579c432cc5a6837e8d9ff5c1bd347e
Daniel de Cloet <djdecloet@gmail.com>
2018-09-18 09:32:02 +0200

    changes cause reserved keywords...

--- EindOpdrachten/introductionJS/js/conditional.js
+++ EindOpdrachten/introductionJS/js/conditional.js
@@ -1,12 +1,11 @@
 // assigning variables
 const num = 1400,
-    str = "Ik woon in Naboo",
-    return = document.getElementById("conditionalReturn")
+    str = "Ik woon in Naboo"
 // comparing string to number
 if (num == str) {
     // this would never be triggered, so could as well be removed
     console.log("The number equals the string... somehow")
 } else {
     // this is the default output.... should always triggered. If statement could be removed
-    console.log("The number equals the string... somehow")
+    console.log("The number does not equal the string, this will always trigger")
 }
