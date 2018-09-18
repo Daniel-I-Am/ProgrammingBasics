9668d83623718fd950c642b46d03886892578957
Daniel de Cloet <djdecloet@gmail.com>
2018-09-18 11:38:38 +0200

    comments

--- finalProducts/typesAndVariables/js/odd.js
+++ finalProducts/typesAndVariables/js/odd.js
@@ -1,17 +1,21 @@
 function submitOdd() {
     const returnP = document.getElementById("oddReturn")
     const input = document.getElementById("oddInput")
+    // if it's not a number
     if (input.value == "") {
         returnP.innerHTML = "Please enter a number"
         return
     }
     if (input.value < 0) {
+        // below 0
         returnP.innerHTML = "Checking if a number below 0 is odd, why would you even think about doing that..."
         return
     }
     if (input.value % 2 == 1) {
+        // odd
         returnP.innerHTML = "This number is odd"
     } else {
+        // even
         returnP.innerHTML = "This number is even"
     }
 }
--- finalProducts/typesAndVariables/js/stringReplace.js
+++ finalProducts/typesAndVariables/js/stringReplace.js
@@ -1,4 +1,4 @@
 let stringReturn = document.getElementById("stringReplaceReturn"),
     inputString = "Programming is not so cool";
-
+// replace not (and the trailing space) with nothing
 stringReturn.innerHTML = inputString.replace("not", "").replace("  ", " ")
