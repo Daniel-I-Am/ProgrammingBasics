145736369172268ce484675564be4d2ab8a2b9a2
Daniel de Cloet <djdecloet@gmail.com>
2018-09-24 11:25:34 +0200

    comments

--- finalProducts/loops/main.js
+++ finalProducts/loops/main.js
@@ -1,3 +1,4 @@
+// recursive fibonacci
 function fib(a,b) {
     console.log(b);
     if (a != Infinity && b != Infinity) {
@@ -5,21 +6,25 @@ function fib(a,b) {
     }
 }
 
+// fibonacci the stupid way, using a while loop
 function fibTheStupidWay() {
     let i = 0,
         a = 0,
         b = 1;
     while (i < 25) {
         console.log(b);
+        // assign the value of a to b and b to a + b in a oneliner, just because we can. 
         b = [a+b, a = b][0];
         i++;
     }
 }
 
+// check if number is divisible by factor
 function div(n, f) {
     return n % f == 0;
 }
 
+// loop through array and check if is divisible by 4
 function deelbaar() {
     let i = 0,
         arr = [4,8,16,20,60,80,24],
@@ -35,6 +40,7 @@ function deelbaar() {
     console.log("Not divisible by 4");
 }
 
+// sum array
 function sum(arr) {
     let total = 0;
     for (let i = arr.length; i > 0; i--) {
