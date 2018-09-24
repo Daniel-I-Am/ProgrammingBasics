537d3559c4b22f5b64ac8d36a63c5ed4dea06d9a
Daniel de Cloet <djdecloet@gmail.com>
2018-09-24 09:51:45 +0200

    training way too basic loops...

---
+++ Training/week39-loops/main.js
@@ -0,0 +1,29 @@
+let i = 0;
+while (i < 10) {
+    i++;
+    console.log(i);
+}
+i = 0
+
+let dagarray = new Array('zondag','maandag','dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
+let date = new Date;
+let dag = dagarray[date.getDay()];
+let string = "Het is " + dag + "!";
+let len = string.length;
+
+while (i < len) {
+    console.log(string.charAt(i))
+    i++;
+}
+
+string = "Geweldig!";
+let newString = "";
+i = string.length;
+
+while (i > 0) {
+    i--;
+    newString += string.charAt(i);
+}
+console.log(newString);
+
+console.log(string.split("").reverse().join(""));
