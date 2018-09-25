cff8e6739396f43404bf5cf3b1e6801ed037197a
Daniel de Cloet <djdecloet@gmail.com>
2018-09-25 10:23:12 +0200

    training week 39

---
+++ Training/week39-functions/main.js
@@ -0,0 +1,31 @@
+// This is isn't declared as `async` because it already returns a promise
+function delay() {
+    // `delay` returns a promise
+    return new Promise(function(resolve, reject) {
+        // Only `delay` is able to resolve or reject the promise
+        setTimeout(function() {
+            resolve(42); // After 3 seconds, resolve the promise with value 42
+        }, 2000);
+    });
+}
+
+async function giveMeSomeNiceName(gender) {
+    let name = 'no name defined';
+    promise = fetch("https://uinames.com/api/?gender=" + gender)
+    .then((response) => {
+      if(response.ok) {
+          return response.json();
+      } else {
+          throw new Error('Server response wasn\'t OK');
+      }
+    })
+    .then((json) => {
+      console.log(json.name)
+    });
+}
+
+function pig(n) {
+    str = ""
+    for (let i = n; n > 0; n--) str += "\u{1F437}"
+    return str;
+}
