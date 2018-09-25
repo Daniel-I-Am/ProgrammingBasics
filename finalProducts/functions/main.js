ba679388adeb1336eeabbd58d42bfc7f43106f9e
Daniel de Cloet <djdecloet@gmail.com>
2018-09-25 11:34:58 +0200

    comments

--- finalProducts/functions/main.js
+++ finalProducts/functions/main.js
@@ -1,15 +1,20 @@
 function fib(a,b) {
+    // if anything's infinity, stop
     if (a == Infinity || b == Infinity) return;
+    // otherwise log our number and call the same function with the next values
     console.log(b);
     fib(b, a+b);
 }
 
 function countdown(n) {
+    // if we don't supply a number, stop
     if (typeof n != "number") return;
+    // if we're at 0, stop and give string
     if (n == 0) {
         let date = new Date()
         console.log(n, "Happy " + date.getFullYear());
     } else {
+        // log number and countdown previous
         console.log(n);
         countdown(n-1);
     }
