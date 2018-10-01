40825fbcaacad20e5350e5736ffc0caa5d629f3c
Daniel de Cloet <djdecloet@gmail.com>
2018-10-01 10:57:22 +0200

    products

---
+++ Training/week40-arrays/main.js
@@ -0,0 +1,8 @@
+let products = [
+    "tandenborster", 
+    "deodorant", 
+    "bakmeel"
+];
+
+console.log("in ieder geval heb ik een deel van het lijstje ondertussen overgenomen...");
+console.table(products);
