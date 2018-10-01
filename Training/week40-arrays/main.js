ac7aafb9e149c529f6dc6171b58f1dd7b9f1d17c
Daniel de Cloet <djdecloet@gmail.com>
2018-10-01 09:27:25 +0200

    filtered lapRound

--- Training/week40-arrays/main.js
+++ Training/week40-arrays/main.js
@@ -4,12 +4,26 @@ console.log(lapRounds[Math.floor(Math.random()*lapRounds.length)]);
 const allMyRecords = [
     ["The Who - Pinball Wizard", "Rolling Stones - Exile on main street", "Police - Message in a bottle"],
     ["De Dijk - Alle 40 Goed", "Het Goede Doel - Belgie", "Doe Maar - skunk"]
- ];
+];
 
- let lp = allMyRecords.length;
- for (let i = 0; i < lp; i++) {
+let lp = allMyRecords.length;
+for (let i = 0; i < lp; i++) {
      let le = allMyRecords[i].length;
      for (let j = 0; j < le; j++) {
          console.log(allMyRecords[i][j]);
      }
- }
+}
+
+const filteredLapRoundsWithForLoop = function () {
+    let newArray = [];
+    for (let i = 0; i < lapRounds.length; i++) {
+ 
+        if (lapRounds[i] < 4) {
+            newArray.push(lapRounds[i]);
+        }
+    }
+    return newArray;
+}
+
+console.log(filteredLapRoundsWithForLoop())
+console.log(lapRounds.filter(time => time < 4));
