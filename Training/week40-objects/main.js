7b236487aff087437cc34c6c8a5e12b824018e5c
Daniel de Cloet <djdecloet@gmail.com>
2018-10-02 09:49:52 +0200

    excercises

---
+++ Training/week40-objects/main.js
@@ -0,0 +1,49 @@
+// me object
+let me = {
+    name: "Daniel",
+    age: 17,
+    previousDiploma: "High School"
+}
+
+console.log(me);
+
+// add value
+me.currentStudy = "HBO-ICT"
+console.log(me)
+
+// random stuff ;p
+function f(x) {
+    console.log(x*x);
+}
+
+let newObj = {
+    children: [
+        me,
+        {
+            name: "You",
+            age: NaN
+        }
+    ],
+    abc1: 1,
+    abc2: 2,
+    someFunc: f,
+    otherFunc: function(x) {
+        console.log(x+1);
+    }
+}
+
+console.log(newObj)
+newObj.someFunc(2)
+newObj.otherFunc(7)
+
+// more stuff
+let transport = {
+    type: "bicycle",
+    wheels: 2
+}
+
+me.transport = transport
+me.pets = ["Fina", "Bonie", "Milou", "Missy", "Castor"]
+
+console.log(`My favorite transport method is ${me.transport.type} and it has ${me.transport.wheels.toString()} wheels`)
+me.pets.forEach(pet => console.log(pet))
