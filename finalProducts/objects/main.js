533392204a5d3ec56d94908fea2c4745510ec74d
Daniel de Cloet <djdecloet@gmail.com>
2018-10-02 11:19:04 +0200

    teachers + salary/hour

--- finalProducts/objects/main.js
+++ finalProducts/objects/main.js
@@ -2,3 +2,28 @@ const lapRounds = {
     lapRounds: [55.99,  63.00, 63.01, 54.01, 62.79, 52.88, 53.10, 54.12]
 };
 
+const teachers = [
+    {
+        name: "Loek",
+        profession: "Teacher",
+        brand: "Linux",
+        salary: NaN,
+        hoursPerWeek: 50
+    },
+    {
+        name: "Daan",
+        profession: "Teacher",
+        brand: "Arduino",
+        salary: 850,
+        hoursPerWeek: 0
+    },
+    {
+        name: "Rimmert",
+        profession: "Teacher",
+        brand: "Apple",
+        salary: 900,
+        hoursPerWeek: 20
+    }
+];
+
+teachers.forEach(obj => console.log(`I have a ${obj.profession} named ${obj.name} and he likes to work on a ${obj.brand} computer. I earn €${obj.salary / obj.hoursPerWeek} per hour`));
