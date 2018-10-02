1ee6aad5f9261fb5c1585bbbfeba09810f620f67
Daniel de Cloet <djdecloet@gmail.com>
2018-10-02 12:26:35 +0200

    bin counter, I guess, it's somethign

--- Training/week40-objects/shenanigans.js
+++ Training/week40-objects/shenanigans.js
@@ -7,3 +7,21 @@ let h =
     {f: function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {if (x) { return function(x) {return x}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 x = 100
 console.log(h.f(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x)(x))
+
+function dec2bin(dec){
+    let s = (dec >>> 0).toString(2);
+    if (s.length < 16) {
+        s = "0".repeat(16-s.length) + s
+    }
+    s = '0b' + s.substring(1, 9) + " " + s.substring(8)
+    return s;
+}
+
+function f(n) {
+    n = n || 0
+    console.log(dec2bin(n))
+    if (n == 11392) {return}
+    f(n+1)
+}
+
+f()
