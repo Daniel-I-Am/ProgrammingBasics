5037437a284c541d615d138dfe9bef810475c9fd
Daniel de Cloet <djdecloet@gmail.com>
2018-10-04 09:38:34 +0200

    readable lines

--- finalProducts/objects/main.js
+++ finalProducts/objects/main.js
@@ -10,23 +10,34 @@ const teachers = [
         profession: "Teacher",
         brand: "Linux",
         salary: NaN,
-        hoursPerWeek: 50
+        hoursPerWeek: 50,
+        salaryPerHour: function() {return this.salary/this.hoursPerWeek}
     },
     {
         name: "Daan",
         profession: "Teacher",
         brand: "Arduino",
         salary: 850,
-        hoursPerWeek: 0
+        hoursPerWeek: 0,
+        salaryPerHour: function() {return this.salary/this.hoursPerWeek}
     },
     {
         name: "Rimmert",
         profession: "Teacher",
         brand: "Apple",
         salary: 900,
-        hoursPerWeek: 20
+        hoursPerWeek: 20,
+        salaryPerHour: function() {return this.salary/this.hoursPerWeek}
     }
 ];
 
 // print sentence, per teacher object
-teachers.forEach(obj => console.log(`I have a ${obj.profession} named ${obj.name} and he likes to work on a${function() { if (obj.brand.toLowerCase().match(/^[aiouey]/)) {return "n"} return ""}()} ${obj.brand} computer. I earn €${obj.salary / obj.hoursPerWeek} per hour`));
+teachers.forEach(obj => console.log(`I have a ${obj.profession} named ${obj.name} and he likes to work on a${
+    function() { if (obj.brand.toLowerCase().match(/^[aiouey]/)) {return "n"} return ""}()} ${
+        obj.brand} computer. I earn €${obj.salaryPerHour()} per hour`));
+
+
+for (let i = 0; i < teachers.length; i++) {
+    const element = teachers[i];
+    console.log(element.salaryPerHour())
+}
